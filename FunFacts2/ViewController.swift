a78acd47a797471309d64598ba5e2c4fe9e1fe32
Nate Bird <natebird@gmail.com>
2016-01-11 15:42:17 -0500

    New project.

---
+++ FunFacts2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts2
+//
+//  Created by Nate Bird on 1/11/16.
+//  Copyright © 2016 Birdhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
