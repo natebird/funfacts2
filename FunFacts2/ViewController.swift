fd7ac7aef99d05d227ff30e7bb9806cb838c1762
Nate Bird <natebird@gmail.com>
2016-01-21 13:54:20 -0500

    Load fact when first loaded. Load second fact when button is pressed.

--- FunFacts2/ViewController.swift
+++ FunFacts2/ViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var funFactLabel: UILabel!
-  
+  let facts = ["Ants stretch when they wake up in the morning", "Ostriches can run faster than horses"]
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    funFactLabel.text = "An interesting fact!"
-
+    funFactLabel.text = facts[0]
   }
 
   override func didReceiveMemoryWarning() {
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
   }
 
   @IBAction func showFunFact() {
-    funFactLabel.text = "Another interesting fact!"
+    funFactLabel.text = facts[1]
   }
 
 }
